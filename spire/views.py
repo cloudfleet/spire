75facb7353b8fc70e55b64140ea1b406ecd13ef4
Dražen Lučanin <kermit666@gmail.com>
2013-11-24 23:10:49 +0100

    logged_in decorator in dashboard view

--- spire/views.py
+++ spire/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     msg = 'Hello cloudfleeters! Welcome to the landing page. Fancy, eh?'
@@ -8,10 +9,20 @@ def home(request):
     context = RequestContext(request)
     return HttpResponse(template.render(context))
 
+@login_required
 def dashboard(request):
     user = request.user
-    if user.is_authenticated():
-        msg = "hello, " + user.username + '. <a href="/account/logout/?next=''">logout</a>'
-        return HttpResponse(msg)
-    else: #TODO: url name instead of path
-        return redirect('/account/login/?next=%s' % request.path)
+    msg = "hello, " + user.username + '. <a href="/account/logout/?next=''">logout</a>'
+    return render(request, 'duralumin/dj-dashboard.html', {
+        'user': user,
+    })
+
+    #return HttpResponse(msg)
+
+    # user = request.user
+    # if user.is_authenticated():
+    #     msg = "hello, " + user.username + '. <a href="/account/logout/?next=''">logout</a>'
+    #     return HttpResponse(msg)
+    # else: #TODO: url name instead of path
+    #     return redirect('/account/login/?next=%s' % request.path)
+    # TODO: different / based on logged in/not
