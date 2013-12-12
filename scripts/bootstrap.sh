ef37367c6d42276c0acbb3393e9d91564a88b584
Dražen Lučanin <kermit666@gmail.com>
2013-12-12 01:22:06 +0100

    different bootstrap.sh parameters

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+# usage: ./bootstrap.sh heroku|dev|prod
+
+
+default=prod
+deploy=${1:-$default}
+
 # TODO:
 # - create venv if not there
 # - install dev dependencies
 
+
 # link to duralumin
+echo linking spire to duralumin
 CURDIR=`pwd`
 cd spire/templates
-if [ -d ../../duralumin ]; then
+rm -f duralumin
+if [[ -d ../../duralumin && ( $deploy = heroku ) ]]; then
     # heroku deployment
+    echo "----> heroku layout"
     ln -s ../../duralumin duralumin
-elif [ -d ../../../duralumin ]; then
+elif [[ -d ../../../duralumin ]]; then
     # dev deployment
+    # - for now not usable, as .scss files not processed
+    if [[ $deploy = "dev" ]]; then
+        echo "----> dev layout"
+        # for live development use app, not dist
+        ln -s ../../../duralumin/app duralumin
     # dist try-out
-    ln -s ../../../duralumin/dist duralumin
-    #TODO: for live development use app, not dist
+    elif [[ $deploy = "prod" ]]; then
+        echo "----> prod layout"
+        ln -s ../../../duralumin/dist duralumin
+    fi
 fi
 cd $CURDIR
