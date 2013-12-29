626e56f004f65350714ad13f4f6d76c053ed0464
Dražen Lučanin <kermit666@gmail.com>
2013-12-29 17:51:20 +0100

    use south for db migrations

--- spire/settings.py
+++ spire/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'django.contrib.sites', TODO: enable, registration recommends it
+    'south',
     'spire.apps.blimps',
 )
 
