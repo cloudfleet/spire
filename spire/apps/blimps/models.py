e1394ca0aa391e9db2a9a84841f4f55860ae45da
Dražen Lučanin <kermit666@gmail.com>
2014-01-03 16:55:47 +0100

    bgtunnel to ssh tunnel

--- spire/apps/blimps/models.py
+++ spire/apps/blimps/models.py
@@ -6,6 +6,16 @@ import docker
 c = docker.Client(base_url='http://localhost:4444',
                   version='1.6',
                   timeout=10)
+import bgtunnel
+import os
+import requests
+
+def start_tunnel():
+    # TODO: put exact details in settings.py
+    forwarder = bgtunnel.open(ssh_user='kermit',
+                                  ssh_address='blimpyard.cloudfleet.io',
+                                  host_port='4243', bind_port='4444',
+                                  identity_file=os.path.expanduser('~/.ssh/blimpyard_rsa'))
 
 class Blimp(models.Model):
     """a magical box that flies over to someone and provides secure,
@@ -22,7 +32,12 @@ class Blimp(models.Model):
     # start blimp
     # sudo docker run -d -p 1338:1337 kermit/hellonode
     def start(self):
-        return c.images()
+        # TODO: use with
+        try:
+            return c.images()
+        except requests.ConnectionError:
+            start_tunnel()
+            return c.images()
 
 class BlimpForm(ModelForm):
     class Meta:
--- spire/wsgi.py
+++ spire/wsgi.py
@@ -10,11 +10,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spire.settings")
 
-# Run startup code!
-import subprocess
-# TODO: put exact details in settings.py
-cmd = 'ssh -o BatchMode=yes -i ~/.ssh/blimpyard_rsa -f -N -L 4444:localhost:4243 kermit@blimpyard.cloudfleet.io'
-process = subprocess.Popen(cmd.split())#, stdout=subprocess.PIPE)
+# Run startup code! - moved to the blimps model using bgtunnel
+#import subprocess
+#cmd = 'ssh -o BatchMode=yes -i ~/.ssh/blimpyard_rsa -f -N -L 4444:localhost:4243 kermit@blimpyard.cloudfleet.io'
+#process = subprocess.Popen(cmd.split())#, stdout=subprocess.PIPE)
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
