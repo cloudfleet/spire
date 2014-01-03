b9c2a93cc43c31480a53f5ccd4b4ee65c32a282f
Dražen Lučanin <kermit666@gmail.com>
2014-01-03 17:05:33 +0100

    not proud of the things I have to do to make this tunneling work :P

--- spire/apps/blimps/models.py
+++ spire/apps/blimps/models.py
@@ -10,12 +10,20 @@ import bgtunnel
 import os
 import requests
 
-def start_tunnel():
+def start_tunnel_once():
     # TODO: put exact details in settings.py
     forwarder = bgtunnel.open(ssh_user='kermit',
-                                  ssh_address='blimpyard.cloudfleet.io',
-                                  host_port='4243', bind_port='4444',
-                                  identity_file=os.path.expanduser('~/.ssh/blimpyard_rsa'))
+                              ssh_address='blimpyard.cloudfleet.io',
+                              host_port='4243', bind_port='4444',
+                              identity_file=os.path.expanduser('~/.ssh/blimpyard_rsa'))
+
+def start_tunnel():
+    try:
+        start_tunnel_once()
+    except bgtunnel.SSHTunnelError:
+        import time
+        time.sleep(3)
+        start_tunnel_once()
 
 class Blimp(models.Model):
     """a magical box that flies over to someone and provides secure,
