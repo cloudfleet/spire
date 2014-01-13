d721f6ac013ca6832a2922714b65b8d8817e3e73
Dražen Lučanin <kermit666@gmail.com>
2014-01-13 23:09:34 +0100

    use plumbum's with statement

--- spire/apps/blimps/models.py
+++ spire/apps/blimps/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 import docker
-c = docker.Client(base_url='http://localhost:4444',
+_docker = docker.Client(base_url='http://localhost:4444',
                   version='1.6',
                   timeout=10)
 import plumbum
 import os
-import requests
+_keyfile = os.path.expanduser('~/.ssh/blimpyard_rsa')
+_blimpyard = plumbum.SshMachine('blimpyard.cloudfleet.io',
+                                user='kermit',
+                                keyfile=_keyfile)
 
 class Blimp(models.Model):
     """a magical box that flies over to someone and provides secure,
@@ -22,19 +25,13 @@ class Blimp(models.Model):
     def __unicode__(self):
         return str("{}'s {}".format(self.owner, self.subdomain))
 
-    def blimpyard_connect(self):
-        keyfile = os.path.expanduser('~/.ssh/blimpyard_rsa')
-        self.blimpyard = plumbum.SshMachine('blimpyard.cloudfleet.io',
-                                            user='kermit',
-                                            keyfile=keyfile)
-        self.blimpyard_tunnel = self.blimpyard.tunnel(4444, 4243)
-
     # start blimp
     # sudo docker run -d -p 1338:1337 kermit/hellonode
     def start(self):
         # TODO: use with
-        self.blimpyard_connect()
-        return c.images()
+        #self.blimpyard_connect()
+        with _blimpyard.tunnel(4444,4243):
+            return _docker.images()
 
 class BlimpForm(ModelForm):
     class Meta:
