2e1ce75aa154c78eab0b34ff31527b5b533b8e24
Dražen Lučanin <kermit666@gmail.com>
2014-01-14 18:13:56 +0100

    blimpyard_tunnel contextmanager

--- spire/apps/blimps/models.py
+++ spire/apps/blimps/models.py
@@ -3,15 +3,25 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 import docker
-_docker = docker.Client(base_url='http://localhost:4444',
+c = docker.Client(base_url='http://localhost:4444',
                   version='1.6',
                   timeout=10)
-import plumbum
+
+from plumbum import SshMachine
 import os
-_keyfile = os.path.expanduser('~/.ssh/blimpyard_rsa')
-_blimpyard = plumbum.SshMachine('blimpyard.cloudfleet.io',
-                                user='kermit',
-                                keyfile=_keyfile)
+from contextlib import contextmanager
+
+@contextmanager
+def blimpyard_tunnel():
+    _keyfile = os.path.expanduser('~/.ssh/blimpyard_rsa')
+    _url = 'blimpyard.cloudfleet.io'
+
+    with SshMachine(_url, user='kermit', keyfile=_keyfile) as _blimpyard_host:
+        with _blimpyard_host.tunnel(4444,4243):
+            try:
+                yield
+            finally:
+                pass
 
 class Blimp(models.Model):
     """a magical box that flies over to someone and provides secure,
@@ -25,25 +35,29 @@ class Blimp(models.Model):
     def __unicode__(self):
         return str("{}'s {}".format(self.owner, self.subdomain))
 
+    # TODO: put these two into some async job queue (celery?)
+
     # start blimp
     # sudo docker run -d -p 1338:1337 kermit/hellonode
     def start(self):
         """start the docker container"""
-        with _blimpyard.tunnel(4444,4243):
+        with blimpyard_tunnel():
             # we get back the container id
-            # TODO: something is still wrong with the container port config
-            container = _docker.create_container('kermit/hellonode',
-                                                 name=self.subdomain)
-            blimp_port = 1338
-            _docker.start(container, port_bindings={1337: ('0.0.0.0', blimp_port)})
-            # TODO: store container info in DB
-            return str(container)
+            container = c.create_container('kermit/hellonode',
+                                            name=self.subdomain,
+                                            ports=[1337])
+            c.start(container, publish_all_ports=True)
+            info = c.inspect_container(container)
+            self.port = info['NetworkSettings']['Ports']['1337'][0]['HostPort']
+            #self.save()
+            print('port is ' + str(self.port))
+            return container
 
     def stop(self):
         """stop the container"""
-        with _blimpyard.tunnel(4444,4243):
-            _docker.stop(self.subdomain)
-            _docker.remove_container(self.subdomain)
+        with blimpyard_tunnel():
+            c.stop(self.subdomain)
+            c.remove_container(self.subdomain)
 
     def url(self):
         # TODO: read from DB
