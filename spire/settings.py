8de81f4934dc528d58cd2309b040cc564447e049
DoubleMalt <christoph@web.crofting.com>
2014-09-15 23:03:13 +0200

    remove non ascii quotes

--- spire/settings.py
+++ spire/settings.py
@@ -111,9 +111,9 @@ TEMPLATE_CONTEXT_PROCESSORS += (
 )
 
 AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of ‘allauth‘
+    # Needed to login by username in Django admin, regardless of 'allauth'
     'django.contrib.auth.backends.ModelBackend',
-    # ‘allauth‘ specific authentication methods, such as login by e-mail
+    # 'allauth' specific authentication methods, such as login by e-mail
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
