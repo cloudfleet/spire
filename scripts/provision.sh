92615477f55417c3eb89f468f8e4150d3fcb447b
Christoph Witzany <christoph@web.crofting.com>
2014-09-16 22:14:14 +0200

    update apt dependencies to python3

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aptitude update
-aptitude install -y git rabbitmq-server redis-server python-virtualenv python-pip cython postgresql-common libpq-dev python3-dev
+aptitude install -y built-essential git rabbitmq-server redis-server python3-virtualenv python3-pip cython cython3 libpq-dev python3-dev
 
 # This is a script to provision a Vagrant machine for development
 (cd /vagrant && scripts/bootstrap.sh vagrant)
