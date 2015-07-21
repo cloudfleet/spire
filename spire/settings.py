a0d8f4bce7c436d1d23f3b0a6767e64e4e3f187d
Dražen Lučanin <kermit666@gmail.com>
2015-07-21 16:14:35 +0200

    add CORS headers

--- spire/settings.py
+++ spire/settings.py
@@ -34,12 +34,14 @@ INSTALLED_APPS = (
     #'allauth.socialaccount',
     'bootstrapform',
     'rest_framework',
+    'corsheaders',
     'spire.apps.blimps',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
