77f299123ac4123e2564cf1835330bda943d21e3
Christoph Witzany <christoph@web.crofting.com>
2017-05-21 15:33:00 +0200

    remove old code that would start docker containers

--- spire/apps/blimps/models.py
+++ spire/apps/blimps/models.py
@@ -8,16 +8,6 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 import requests
 
-import docker
-docker_base_url = 'http://{}:{}'.format(
-    settings.SPIRE_DOCKER_API_HOST,
-    settings.SPIRE_DOCKER_API_PORT
-)
-c = docker.Client(base_url=docker_base_url,
-                  version='1.6',
-                  timeout=10)
-
-from plumbum import SshMachine
 import os
 from contextlib import contextmanager
 
@@ -25,43 +15,6 @@ from contextlib import contextmanager
 
 from . import lib
 
-
-@contextmanager
-def blimpyard_tunnel():
-    try:
-        keyfile = os.path.expanduser(settings.BLIMPYARD_KEY)
-    except AttributeError:
-        keyfile = None
-    url = settings.BLIMPYARD_URL
-    user = settings.BLIMPYARD_USER
-    docker_port = settings.DOCKER_PORT
-    local_port = settings.SPIRE_DOCKER_API_PORT
-
-    with SshMachine(url, user=user, keyfile=keyfile) as blimpyard_host:
-        with blimpyard_host.tunnel(local_port, docker_port):
-            try:
-                yield blimpyard_host
-            finally:
-                pass
-
-def get_pagekite_frontend():
-    if settings.BLIMPYARD_URL == 'localhost':
-        cmd = "./scripts/docker_host_ip.sh"
-        url = subprocess.check_output(cmd).decode().rstrip()
-    else:
-        url = settings.BLIMPYARD_URL
-    port = settings.BLIMPYARD_PAGEKITE_PORT
-    if port != 80:
-        url = url + ':' + str(port)
-    return url
-
-# push notification to the frontend - work in progress
-# r = redis.StrictRedis(host='localhost', port=6379, db=0)
-# def push_notification():
-#     """push notofication to frontend using Redis + Node.js"""
-#     # TODO: add blimp id data to message
-#     # TODO: test with Celery in another process
-#     r.publish('spire-pusher', 'blimp ready')
 
 class Blimp(models.Model):
     """a magical box that flies over to someone and provides secure,
@@ -104,66 +57,6 @@ class Blimp(models.Model):
                     .format(blimp.host())
                 )
 
-    # start blimp
-    # sudo docker run -d -p 3001:3000 cloudfleet/cockpit
-    def start(self):
-        """start the docker container"""
-        with blimpyard_tunnel() as rem:
-            # start container
-            logging.info('1. start container')
-            try:
-                pagekite_frontend = get_pagekite_frontend()
-                secret = 'password' # TODO: randomly generate
-                # test.localhost password 172.17.42.1:60666
-                # we get back the container id
-
-                environment = {
-                    "CLOUDFLEET_DOMAIN": self.host(),
-                    "CLOUDFLEET_SECRET": secret,
-                    "CLOUDFLEET_HOST": pagekite_frontend
-                }
-                logging.debug(' - environment: ' + str(environment))
-
-                container = c.create_container(settings.DOCKER_IMAGE,
-                                               name=self.subdomain,
-                                               environment=environment)
-                c.start(container)
-            except requests.exceptions.ConnectionError:
-                logging.error(" - didn't start: connection error")
-                container = None
-            else:
-                logging.info(' - {} container started'.format(settings.DOCKER_IMAGE))
-                info = c.inspect_container(container)
-                self.port = info['NetworkSettings']['Ports']['3000/tcp'][0]['HostPort']
-                self.save(update_fields=['port'])
-                logging.info('- docker exposed url: ' + self.url_exposed())
-
-            # update backends.rc
-            logging.info('2. update backends.rc')
-            backendsrc_path = 'backends.rc'
-            self.generate_backendsrc(backendsrc_path)
-            rem.upload(backendsrc_path, '/etc/pagekite.d/20_backends.rc')
-
-            # restart pagekite frontend
-            logging.info('3. restart pagekite')
-            # - call the Flask service that restarts pagekite
-            rem['/usr/bin/wget localhost:5000 -o /dev/null']()
-
-            # notify frontend to update the view
-            #push_notification()
-            return container
-
-    def stop(self):
-        """stop the container"""
-        with blimpyard_tunnel():
-            try:
-                logging.debug('1. stop container')
-                c.stop(self.subdomain)
-                logging.debug('2. remove container')
-                c.remove_container(self.subdomain)
-                logging.info('- blimp deleted')
-            except requests.exception.ConnectionError:
-                logging.error('Connection error')
 
     def url_exposed(self):
         """as exposed through Docker"""
